d1759d8d74231b63da8da8cb0da89b640607f532
AlexandruTajamar21 <alexandru.preutesei@tajamar365.com>
2022-05-06 10:57:18 +0200

    commit

---
+++ MvcAWSCacheRedis/Controllers/ProductosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcAWSCacheRedis.Models;
+using MvcAWSCacheRedis.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcAWSCacheRedis.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private RepositoryProductos repo;
+
+        public ProductosController(RepositoryProductos repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult IndexProductos()
+        {
+            List<Producto> productos = this.repo.GetProductos();
+            return View(productos);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Producto produc = this.repo.FindProducto(id);
+            return View(produc);
+        }
+    }
+}
